12d89845f74b71329a57b40cb740c5995fa13c71
smolkovkonstantin <konstantin.w.smolkov@gmail.com>
2024-04-21 14:52:49 +0300

    init

---
+++ src/main/java/org/online/queue/onlinequeuejwt/OnlineQueueJwtApplication.java
@@ -0,0 +1,13 @@
+package org.online.queue.onlinequeuejwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineQueueJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineQueueJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/online/queue/onlinequeuejwt/OnlineQueueJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.online.queue.onlinequeuejwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineQueueJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
